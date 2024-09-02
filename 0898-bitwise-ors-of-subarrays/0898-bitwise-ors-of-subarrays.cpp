ab3856cd32defa9f809247e3967f349211a48de2
Sumit Kumar <skumar21-comp@bvucoep.edu.in>
2024-09-02 12:07:30 +0530

    Time: 967 ms (67.98%), Space: 219.4 MB (68.25%) - LeetHub

---
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+        int subarrayBitwiseORs(vector<int> A) {
+        unordered_set<int> res, cur, cur2;
+        for (int i: A) {
+            cur2 = {i};
+            for (int j: cur) cur2.insert(i|j);
+            for (int j: cur = cur2) res.insert(j);
+        }
+        return res.size();
+    }
+
+};
